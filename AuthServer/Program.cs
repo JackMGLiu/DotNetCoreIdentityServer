949c9f8dbfdd274aef9f26c6d4bc6be229f9c1b0
Jack-Liu <love_zs1314@126.com>
2018-08-17 09:46:03 +0800

    修改启动配置

--- AuthServer/Program.cs
+++ AuthServer/Program.cs
@@ -19,6 +19,8 @@ namespace AuthServer
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseUrls("http://*.5000")
                 .UseStartup<Startup>()
                 .Build();
     }
