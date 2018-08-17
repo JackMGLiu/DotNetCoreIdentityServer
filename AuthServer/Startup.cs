5540a33d358a757955556da7eee66e8a96da6873
Jack-Liu <love_zs1314@126.com>
2018-08-17 15:46:10 +0800

    完成配置文件的ef移植

--- AuthServer/Startup.cs
+++ AuthServer/Startup.cs
@@ -1,31 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using AuthServer.Configuration;
+using IdentityServer4.EntityFramework.DbContexts;
+using IdentityServer4.EntityFramework.Mappers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace AuthServer
 {
     public class Startup
     {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+            var migrationsAssembly = typeof(Startup).GetTypeInfo().Assembly.GetName().Name;
+
             //配置 identityserver
             //1.哪些API可以使用这个authorization server.
             //2.那些客户端Client(应用)可以使用这个authorization server.
             //3.指定可以使用authorization server授权的用户.
             services.AddIdentityServer()
-                .AddDeveloperSigningCredential()
-                .AddInMemoryIdentityResources(IdentityServerConfig.GetIdentityResources())
-                .AddInMemoryApiResources(IdentityServerConfig.ApiResources())
-                .AddInMemoryClients(IdentityServerConfig.Clients())
-                .AddTestUsers(IdentityServerConfig.Users().ToList());
+                   //.AddDeveloperSigningCredential()
+                   //.AddInMemoryIdentityResources(IdentityServerConfig.GetIdentityResources())
+                   //.AddInMemoryApiResources(IdentityServerConfig.ApiResources())
+                   //.AddInMemoryClients(IdentityServerConfig.Clients())
+                   //.AddTestUsers(IdentityServerConfig.Users().ToList());
+                   .AddDeveloperSigningCredential()
+                   .AddTestUsers(IdentityServerConfig.Users().ToList())
+                   .AddConfigurationStore(options =>
+                   {
+                       options.ConfigureDbContext = builder =>
+                            builder.UseSqlServer(connectionString, sql => sql.MigrationsAssembly(migrationsAssembly));
+                   })
+                   // this adds the operational data from DB (codes, tokens, consents)
+                   .AddOperationalStore(options =>
+                    {
+                        options.ConfigureDbContext = builder =>
+                            builder.UseSqlServer(connectionString,
+                                sql => sql.MigrationsAssembly(migrationsAssembly));
+
+                        // this enables automatic token cleanup. this is optional.
+                        options.EnableTokenCleanup = true;
+                        options.TokenCleanupInterval = 30;
+                    });
+
 
             services.AddMvc();
         }
@@ -33,6 +68,8 @@ namespace AuthServer
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
+            InitializeDatabase(app);
+
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -44,6 +81,43 @@ namespace AuthServer
             app.UseStaticFiles();
 
             app.UseMvcWithDefaultRoute();
+        }
+
+        private void InitializeDatabase(IApplicationBuilder app)
+        {
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                serviceScope.ServiceProvider.GetRequiredService<PersistedGrantDbContext>().Database.Migrate();
+
+                var context = serviceScope.ServiceProvider.GetRequiredService<ConfigurationDbContext>();
+                context.Database.Migrate();
+                if (!context.Clients.Any())
+                {
+                    foreach (var client in IdentityServerConfig.Clients())
+                    {
+                        context.Clients.Add(client.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.IdentityResources.Any())
+                {
+                    foreach (var resource in IdentityServerConfig.GetIdentityResources())
+                    {
+                        context.IdentityResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+
+                if (!context.ApiResources.Any())
+                {
+                    foreach (var resource in IdentityServerConfig.ApiResources())
+                    {
+                        context.ApiResources.Add(resource.ToEntity());
+                    }
+                    context.SaveChanges();
+                }
+            }
         }
     }
 }
