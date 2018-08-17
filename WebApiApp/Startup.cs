da41f78893c46d2a3f213bbb4073b3c00002529a
Jack-Liu <love_zs1314@126.com>
2018-08-17 10:48:03 +0800

    添加权限配置属性

--- WebApiApp/Startup.cs
+++ WebApiApp/Startup.cs
@@ -24,7 +24,23 @@ namespace WebApiApp
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            //IdentityServer4.AccessTokenValidation配置
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddJsonFormatters();
+
+            //这里AddAuthentication()是把验证服务注册到DI, 并配置了Bearer作为默认模式.
+            //AddIdentityServerAuthentication()是在DI注册了token验证的处理者.
+            //由于是本地运行, 所以就不使用https了, RequireHttpsMetadata = false.如果是生产环境, 一定要使用https.
+            //Authority指定Authorization Server的地址.
+            //ApiName要和Authorization Server里面配置ApiResource的name一样.
+            //然后, 在Startup的Configure方法里配置Authentication中间件.
+            services.AddAuthentication("Bearer")
+                .AddIdentityServerAuthentication(options => {
+                    options.RequireHttpsMetadata = false;
+                    options.Authority = "http://localhost：5000/";
+                    options.ApiName = "demonetwork1";
+                });
 
             //注册 Swagger
             services.AddSwaggerGen(c =>
@@ -50,6 +66,7 @@ namespace WebApiApp
                 c.SwaggerEndpoint("/swagger/v1/swagger.json", "WebApiApp");
             });
 
+            app.UseAuthentication();
 
             app.UseMvc();
         }
