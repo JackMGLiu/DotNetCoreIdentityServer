8cd10c5c9f8b2182f8546160e281ed4d1a7d1cbf
Jack-Liu <love_zs1314@126.com>
2018-08-21 10:00:35 +0800

    新增用户类及相关业务逻辑

---
+++ Identity4SSO/Models/Service/IAdminService.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Tasks;
+
+namespace Identity4SSO.Models.Service
+{
+    public interface IAdminService
+    {
+        /// <summary>
+        /// 根据用户名和密码查找用户
+        /// </summary>
+        /// <param name="username">用户名</param>
+        /// <param name="pwd">用户密码</param>
+        /// <returns></returns>
+        Task<Admin> GetAdminModel(string username, string pwd);
+    }
+}
---
+++ Identity4SSO/Models/Service/Impl/AdminService.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity4SSO.Models.Service.Impl
+{
+    public class AdminService: IAdminService
+    {
+        public EFContext _db;
+        public AdminService(EFContext db)
+        {
+           this._db = db;
+        }
+
+        public async Task<Admin> GetAdminModel(string username, string pwd)
+        {
+            var model = await _db.Admin.SingleOrDefaultAsync(m => m.UserName == username && m.Password == pwd);
+            if (model == null)
+            {
+                return null;
+            }
+            else
+            {
+                return model;
+            }
+        }
+    }
+}
